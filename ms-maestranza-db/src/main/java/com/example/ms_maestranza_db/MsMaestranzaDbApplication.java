04efa5df9ca298f4cce03e687461b9e0d04c0ccb
joselyn gallardo <jo.gallardo@duocuc.cl>
2025-06-24 10:47:21 -0400

    agreagar

---
+++ ms-maestranza-db/src/main/java/com/example/ms_maestranza_db/MsMaestranzaDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_maestranza_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMaestranzaDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMaestranzaDbApplication.class, args);
+	}
+
+}
---
+++ ms-maestranza-db/src/test/java/com/example/ms_maestranza_db/MsMaestranzaDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_maestranza_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMaestranzaDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
